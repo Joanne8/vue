ae482715d60ed2452d059668f6d824c98d2d13b2
Joanne <Joanne8@users.noreply.github.com>
2019-10-08 08:38:07 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <v-address :seller="seller"></v-address>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">点餐</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">更多</router-link>
+      </div>
+    </div>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {urlParse} from 'common/js/util';
+  import header from 'components/header/header.vue';
+  import address from 'components/address/address.vue';
+
+  const ERR_OK = 0;
+
+  export default {
+    data() {
+      return {
+        seller: {
+          id: (() => {
+            let queryParam = urlParse();
+            return queryParam.id;
+          })()
+        }
+      };
+    },
+    created() {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = Object.assign({}, this.seller, response.data);
+        }
+      });
+    },
+    components: {
+      'v-header': header,
+      'v-address': address
+    }
+  };
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: #666
+        &.active
+          color: #522C0F
+          font-size:15px
+          font-weight:bold
+</style>
---
+++ src/components/address/address.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="address-box border-1px" v-if="seller.infos">
+    <span class="addrIcon"></span>
+    <div class="addrText">
+      <div class="addr">{{seller.infos[2]}}</div>
+      <div class="addrLine">{{seller.infos[3]}}</div>
+    </div>
+    <div class="telPhone">
+      <img src="./phone.png" alt="">
+      <a :href="'tel:'+seller.telephone" class="contact">联系商家</a>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  };
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
+
+  .address-box
+    display:flex
+    justify-content: space-between
+    padding-bottom:12px
+    margin: 12px 12px 0
+    border-1px(rgba(7, 17, 27, 0.1))
+    .addrIcon
+      display: inline-block
+      vertical-align: top
+      width: 16px
+      height: 16px
+      bg-image('addr')
+      background-size: contain
+      background-repeat: no-repeat
+      opacity: 0.7
+    .addrText
+      line-height: 20px
+      border-right: 1px solid rgba(7,17,27,0.1)
+      padding-right: 10px
+      .addr
+        color: #522C0F
+        font-size: 16px
+        overflow: hidden
+        text-overflow: ellipsis
+        white-space: nowrap
+        width: 254px
+      .addrLine
+        color: #999
+        font-size: 12px;
+    .telPhone
+      img
+        width: 20px
+        height: 20px
+        margin: 0 auto 3px
+        display: block
+      .contact
+        font-size: 10px
+        color: #999
+</style>
